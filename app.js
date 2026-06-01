ad527950e0a8c294b6f1f1fed259b41378455739
Bluegnarl <bluegnarl@gmail.com>
2026-06-01 11:38:29 +0200

    Fix

--- app.js
+++ app.js
@@ -43,6 +43,15 @@ const app = express();
 //     }),
 // );
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    if (req.method === "OPTIONS") return res.sendStatus(204);
+    next();
+});
+
 app.use(cors({ origin: true, credentials: true }));
 
 app.use(express.json());
